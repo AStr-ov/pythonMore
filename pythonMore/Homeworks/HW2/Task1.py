10842de8f71dd4a2b375529263c75143edb63b96
Astr <boxreg2000@yandex.ru>
2023-03-04 12:03:52 +0300

    HW2, task1 Проще

--- pythonMore/Homeworks/HW2/Task1.py
+++ pythonMore/Homeworks/HW2/Task1.py
@@ -1,32 +1,28 @@
 '''Напишите программу, которая получает целое число и возвращает его
 шестнадцатеричное строковое представление.
 Функцию hex используйте для проверки своего результата.'''
-import math
-
 number = int(input("Введите число: "))
 NUMBER = number
 SYS = 16
-a, b = divmod(number, SYS)
 result = ''
-while number % SYS:
-    if b == 10:
-        t = 'a'
-    elif b == 11:
-        t = 'b'
-    elif b == 12:
-        t = 'c'
-    elif b == 13:
-        t = 'd'
-    elif b == 14:
-        t = 'e'
-    elif b == 15:
-        t = 'f'
+while number > 0:
+    rem = number % SYS
+    if rem == 10:
+        letter = 'a'
+    elif rem == 11:
+        letter = 'b'
+    elif rem == 12:
+        letter = 'c'
+    elif rem == 13:
+        letter = 'd'
+    elif rem == 14:
+        letter = 'e'
+    elif rem == 15:
+        letter = 'f'
     else:
-        t = str(b)
-    result = t + result
-    number = a
-    a, b = divmod(number, SYS)
-
+        letter = str(rem)
+    result = str(letter) + result
+    number = number // SYS
 print('Шестнадцатеричное строковое представление числа',NUMBER,': 0x' + result)
 print("Bычислениe функции hex(",NUMBER,")                       :",hex(NUMBER))
 print("Ответ программы соответствует вычислению функции hex()" if ('0x' + result) == hex(NUMBER) else
