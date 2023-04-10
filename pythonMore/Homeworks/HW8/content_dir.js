414f723fb3aea9020ff938ded5a0a1eda75b2a6e
Astr <boxreg2000@yandex.ru>
2023-04-10 18:29:43 +0300

    HW8, task1

---
+++ pythonMore/Homeworks/HW8/content_dir.js
@@ -0,0 +1,75 @@
+{
+  "Name": "Bots",
+  "Type": "DIR",
+  "Parent": "For Python"
+}{
+  "Name": "001",
+  "Type": "DIR",
+  "Parent": "Bots"
+}{
+  "Name": "002",
+  "Type": "DIR",
+  "Parent": "Bots"
+}{
+  "Name": "first bot",
+  "Type": "DIR",
+  "Parent": "Bots"
+}{
+  "Name": "proba",
+  "Type": "DIR",
+  "Parent": "Bots"
+}{
+  "Name": "00.py",
+  "Type": "FILE",
+  "Parent": "Bots",
+  "Size": "824 bytes"
+}{
+  "Name": "002.py",
+  "Type": "FILE",
+  "Parent": "Bots",
+  "Size": "357 bytes"
+}{
+  "Name": "003.py",
+  "Type": "FILE",
+  "Parent": "Bots",
+  "Size": "866 bytes"
+}{
+  "Name": "04.py",
+  "Type": "FILE",
+  "Parent": "Bots",
+  "Size": "898 bytes"
+}{
+  "Name": "bot.py",
+  "Type": "FILE",
+  "Parent": "001",
+  "Size": "346 bytes"
+}{
+  "Name": "__pycache__",
+  "Type": "DIR",
+  "Parent": "002"
+}{
+  "Name": "commands.py",
+  "Type": "FILE",
+  "Parent": "002",
+  "Size": "537 bytes"
+}{
+  "Name": "main.py",
+  "Type": "FILE",
+  "Parent": "002",
+  "Size": "485 bytes"
+}{
+  "Name": "commands.cpython-38.pyc",
+  "Type": "FILE",
+  "Parent": "__pycache__",
+  "Size": "473 bytes"
+}{
+  "Name": "main.py",
+  "Type": "FILE",
+  "Parent": "first bot",
+  "Size": "434 bytes"
+}{
+  "Name": "main.py",
+  "Type": "FILE",
+  "Parent": "proba",
+  "Size": "534 bytes"
+}
